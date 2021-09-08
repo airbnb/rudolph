a38e01da6ecde2e80abced4e9b49d5d4b494b3f8
Ryxias <derek.wang@airbnb.com>
2021-09-08 13:19:51 -0700

    Updates to the csv import/export (#4)
    * adding support for cocurrent dynamodb writes during csv rule import
    
    * it's a me
    
    Co-authored-by: Ryan Deivert <ryan.deivert@airbnb.com>

--- internal/cli/rules/rules-import.go
+++ internal/cli/rules/rules-import.go
@@ -3,6 +3,7 @@ package rules
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 
 	"github.com/spf13/cobra"
 
@@ -14,8 +15,16 @@ import (
 	"github.com/airbnb/rudolph/pkg/types"
 )
 
+// consts for "workers" in pool for dynamodb operations
+const (
+	defaultWorkers = 10
+	minWorkers     = 1
+	maxWorkers     = 2 << defaultWorkers // 2048 default, relative to defaultWorkers
+)
+
 func addRuleImportCommand() {
 	var filename string
+	var workers int
 
 	var ruleImportCmd = &cobra.Command{
 		Use:     "import <file-name>",
@@ -29,11 +38,18 @@ func addRuleImportCommand() {
 
 			dynamodbClient := dynamodb.GetClient(table, region)
 
-			return runImport(dynamodbClient, clock.ConcreteTimeProvider{}, filename)
+			// Try to prevent stupidity
+			if workers < minWorkers || workers > maxWorkers {
+				fmt.Printf("[WARNING] invalid worker count (%d); using default: %d\n", workers, defaultWorkers)
+				workers = defaultWorkers
+			}
+
+			return runImport(dynamodbClient, clock.ConcreteTimeProvider{}, filename, workers)
 		},
 	}
 
 	ruleImportCmd.Flags().StringVarP(&filename, "filename", "f", "", "The filename")
+	ruleImportCmd.Flags().IntVarP(&workers, "workers", "w", defaultWorkers, "Number of workers")
 	_ = ruleImportCmd.MarkFlagRequired("filename")
 
 	RulesCmd.AddCommand(ruleImportCmd)
@@ -43,84 +59,105 @@ func runImport(
 	client dynamodb.DynamoDBClient,
 	timeProvider clock.TimeProvider,
 	filename string,
-) (err error) {
-	csvLines := make(chan map[string]string)
+	numWorkers int,
+) error {
 
-	wg := new(sync.WaitGroup)
-
-	go func() {
-		for {
-			line := <-csvLines
-
-			sha256, ok := line["sha256"]
-			if !ok {
-				panic("no sha256")
-			}
-			ruleTypeStr, ok := line["type"]
-			if !ok {
-				panic("no type")
-			}
-			policyStr, ok := line["policy"]
-			if !ok {
-				panic("no policy")
-			}
-			description, ok := line["description"]
-			if !ok {
-				description = ""
-			}
-			var ruleType types.RuleType
-			err := ruleType.UnmarshalText([]byte(ruleTypeStr))
-			if err != nil {
-				panic("invalid ruletype")
-			}
-			var policy types.Policy
-			err = policy.UnmarshalText([]byte(policyStr))
-			if err != nil {
-				panic("invalid policy")
-			}
-
-			suffix := ""
-			if ruleType == types.Certificate {
-				suffix = " (Cert)"
-			}
-
-			if policy == types.RulePolicyRemove {
-				fmt.Printf("  Removing rule: [%s]\n", sha256)
-				sortkey := rudolphrules.RuleSortKeyFromTypeSHA(sha256, ruleType)
-				err = globalrules.RemoveGlobalRule(
-					timeProvider,
-					client,
-					client,
-					sortkey,
-					"",
-				)
-
-			} else {
-				fmt.Printf("  Writing rule: [%s] %s%s\n", policyStr, sha256, suffix)
-				err = globalrules.AddNewGlobalRule(
-					timeProvider,
-					client,
-					sha256,
-					ruleType,
-					policy,
-					description,
-				)
-			}
-
-			if err != nil {
-				panic(err)
-			}
-
-			wg.Done()
-		}
-	}()
-
-	err = csv.ParseCsvFile(filename, csvLines, wg)
+	// ParseCsvFile returns a data channel and an optional error if any issues
+	// occurred while opening the file for reading
+	data, err := csv.ParseCsvFile(filename)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
+	// Track a total number of lines processed
+	// This gets passed to workers and atomic.Add is
+	// used to increment in a thread-safe way
+	var total uint64
+
+	// Start the workers
+	var wg sync.WaitGroup
+	for w := 0; w < numWorkers; w++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done() // ensure Done is called after this worker is complete
+			ddbWriter(client, timeProvider, data, &total)
+		}()
+	}
+
+	// chill
 	wg.Wait()
 
+	fmt.Println("processed lines:", total)
+
 	return nil
+}
+
+func ddbWriter(
+	client dynamodb.DynamoDBClient,
+	timeProvider clock.TimeProvider,
+	lines chan map[string]string,
+	total *uint64,
+) {
+	for line := range lines {
+		atomic.AddUint64(total, 1)
+
+		sha256, ok := line["sha256"]
+		if !ok {
+			panic("no sha256")
+		}
+		ruleTypeStr, ok := line["type"]
+		if !ok {
+			panic("no type")
+		}
+		policyStr, ok := line["policy"]
+		if !ok {
+			panic("no policy")
+		}
+		description, ok := line["description"]
+		if !ok {
+			description = ""
+		}
+		var ruleType types.RuleType
+		err := ruleType.UnmarshalText([]byte(ruleTypeStr))
+		if err != nil {
+			panic("invalid ruletype")
+		}
+		var policy types.Policy
+		err = policy.UnmarshalText([]byte(policyStr))
+		if err != nil {
+			panic("invalid policy")
+		}
+
+		suffix := ""
+		if ruleType == types.Certificate {
+			suffix = " (Cert)"
+		}
+
+		if policy == types.RulePolicyRemove {
+			fmt.Printf("  Removing rule: [%s]\n", sha256)
+			sortkey := rudolphrules.RuleSortKeyFromTypeSHA(sha256, ruleType)
+			err = globalrules.RemoveGlobalRule(
+				timeProvider,
+				client,
+				client,
+				sortkey,
+				"",
+			)
+
+		} else {
+			fmt.Printf("  Writing rule: [%s] %s%s\n", policyStr, sha256, suffix)
+			err = globalrules.AddNewGlobalRule(
+				timeProvider,
+				client,
+				sha256,
+				ruleType,
+				policy,
+				description,
+			)
+		}
+
+		if err != nil {
+			panic(err)
+		}
+	}
 }
--- internal/csv/parse.go
+++ internal/csv/parse.go
@@ -3,6 +3,7 @@ package csv
 import (
 	"encoding/csv"
 	"fmt"
+	"io"
 	"os"
 	"sync"
 
@@ -14,49 +15,47 @@ import (
 // * have a header row
 // * be valid csv (consistent number of columns per row)
 // * be newline delimited
-func ParseCsvFile(
-	filepath string,
-	output chan<- map[string]string,
-	wg *sync.WaitGroup,
-) (err error) {
+func ParseCsvFile(filepath string) (output chan map[string]string, err error) {
+	output = make(chan map[string]string)
+
 	f, err := os.Open(filepath)
 	if err != nil {
 		err = errors.Wrapf(err, "failed to open file: %s", filepath)
 		return
 	}
-	defer f.Close() // this needs to be after the err check
 
-	lines, err := csv.NewReader(f).ReadAll()
+	reader := csv.NewReader(f)
+	header, err := reader.Read()
 	if err != nil {
 		err = errors.Wrapf(err, "failed to read csv file: %s", filepath)
 		return
 	}
 
-	if len(lines) == 0 {
-		err = errors.New("no lines detected in csv file")
-		return
-	}
+	go func() {
+		defer f.Close() // this needs to be done here, or will result in error reading a closed file
+		for {
+			line, err := reader.Read()
+			if err == io.EOF {
+				break // reached EOF
+			} else if err != nil {
+				panic(err) // This is likely an unrecoverable error, so just quit
+			}
 
-	var header []string
-	for lineNumber, line := range lines {
-		if lineNumber == 0 {
-			header = line
-			continue
+			if len(line) != len(header) {
+				// Log the error. consider using a struct with err atttribute, or
+				// list of errors, to track overall errors
+				fmt.Println("[ERROR] incorrect number of columns in line:", line)
+				continue
+			}
+			data := make(map[string]string, len(line))
+			for columnNumber, item := range line {
+				data[header[columnNumber]] = item
+			}
+
+			output <- data
 		}
-
-		if len(line) != len(header) {
-			err = errors.New(fmt.Sprintf("incorrect number of columns in line: %s", line))
-			return
-		}
-
-		data := make(map[string]string, len(line))
-		for columnNumber, item := range line {
-			data[header[columnNumber]] = item
-		}
-
-		wg.Add(1)
-		output <- data
-	}
+		close(output) // Calling close here ensures workers exit properly
+	}()
 
 	return
 }
@@ -65,16 +64,15 @@ func WriteCsvFile(
 	filename string,
 	headers []string,
 	input <-chan []string,
-	wg *sync.WaitGroup,
-) (err error) {
+) (wg *sync.WaitGroup, err error) {
 	f, err := os.Create(filename)
 	if err != nil {
 		err = errors.Wrapf(err, "failed to open file for writing: %s", filename)
 		return
 	}
-	defer f.Close()
 	err = f.Truncate(0)
 	if err != nil {
+		f.Close()
 		err = errors.Wrap(err, "file is not writable")
 		return
 	}
@@ -87,16 +85,21 @@ func WriteCsvFile(
 		return
 	}
 
-	for {
-		record := <-input
+	wg = new(sync.WaitGroup)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		defer f.Close()
+		defer csvWriter.Flush()
 
-		err = csvWriter.Write(record)
-		if err != nil {
-			err = errors.Wrap(err, "could not write csv line")
-			return
+		for record := range input {
+			err = csvWriter.Write(record)
+			if err != nil {
+				err = errors.Wrap(err, "could not write csv line")
+				return
+			}
 		}
+	}()
 
-		csvWriter.Flush()
-		wg.Done()
-	}
+	return
 }
