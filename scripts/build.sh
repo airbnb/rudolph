3893706e40e6690a92f4dd77ba102ebf98b0e051
Ryan Diers <39590744+radsec@users.noreply.github.com>
2024-03-28 13:57:04 -0700

    [TF] Update Lambda versioning/aliases logic, tune Lambdas, and AWS firehose TF changes (#43)
    * update lambda aliases
    
    * tune lambda memory
    
    * remove this broken symbolic link during builds
    
    * reduce warnings and migrate the firehose S3 TF resources

--- scripts/build.sh
+++ scripts/build.sh
@@ -42,7 +42,6 @@ if [ "$(uname)" == "Darwin" ]; then
 else
     echo "  compiling cli..."
     go build -o $CLI_BUILD_DIR/cli $APPS_DIR/cli
-    ln -sf $CLI_BUILD_DIR/cli $DIR/$CLI_NAME
 fi
 
 echo "*** packaging... ***"
